a9386f4591233c1499aaf4e1c460d21ead23ec30
shadow2105 <ashwinrajput66@gmail.com>
2023-10-06 09:18:07 -0400

    Initial Project Add

---
+++ src/main/java/com/example/ebsclouddemo/EbsCloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebsclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbsCloudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbsCloudDemoApplication.class, args);
+    }
+
+}
